62953bc2630c31bf4978315cc6e45e5f937efe70
ACH2022DEV <abdaziz.charfi@gmail.com>
2022-08-21 21:00:17 +0200

    [ACH] : Upload and retrieve Images1

--- src/main/java/com/pfa/ecommerce/repository/ImagesRepository.java
+++ src/main/java/com/pfa/ecommerce/repository/ImagesRepository.java
@@ -5,5 +5,5 @@ import com.pfa.ecommerce.entities.ImagesEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ImagesRepository extends JpaRepository<ImagesEntity,Long> {
-
+//
 }
